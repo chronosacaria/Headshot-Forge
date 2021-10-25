edf96cf5ce32a1a607740b5b5ac13e86cddb8597
chronosacaria <67794130+chronosacaria@users.noreply.github.com>
2021-10-25 16:28:48 -0400

    Updated to 1.17.1

--- src/main/java/chronosacaria/headshot/Headshot.java
+++ src/main/java/chronosacaria/headshot/Headshot.java
@@ -6,7 +6,6 @@ import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Headshot.MOD_ID)
 public class Headshot {
--- src/main/java/chronosacaria/headshot/events/DealHeadshotEvent.java
+++ src/main/java/chronosacaria/headshot/events/DealHeadshotEvent.java
@@ -2,121 +2,93 @@ package chronosacaria.headshot.events;
 
 import chronosacaria.headshot.Headshot;
 import chronosacaria.headshot.config.HeadshotConfig;
-import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.enchantment.Enchantments;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.boss.dragon.EnderDragonEntity;
-import net.minecraft.entity.monster.SlimeEntity;
-import net.minecraft.entity.passive.AnimalEntity;
-import net.minecraft.entity.passive.WaterMobEntity;
-import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.inventory.EquipmentSlotType;
-import net.minecraft.potion.EffectInstance;
-import net.minecraft.potion.Effects;
-import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.animal.WaterAnimal;
+import net.minecraft.world.entity.boss.enderdragon.EnderDragon;
+import net.minecraft.world.entity.monster.Slime;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = Headshot.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class DealHeadshotEvent {
+
     @SubscribeEvent
     public static void onHeadshotIfApplicable(LivingDamageEvent event){
+        if (event.getSource().isProjectile()){
+            Entity trueSource = event.getSource().getEntity();
 
-        boolean ignore = false;
-
-        if (event.getSource().isProjectile()) {
-            // Firing Entity or Projectile in the case of no True Source
-            Entity trueSource = event.getSource().getTrueSource();
-
-            // Target Entity
             Entity entity = event.getEntityLiving();
 
-            // Isolation of the head of bipedal mobs
-            double headStart = entity.getPositionVec().add(0.0, entity.getSize(entity.getPose()).height * 0.85, 0.0).y - 0.17;
+            double headStart =
+                    entity.position().add(0.0, entity.getDimensions(entity.getPose()).height * 0.85, 0.0).y - 0.17;
 
-            // Determining if the target is wearing a helmet. If the target does not have a helmet on, do X
-            if (!ignore && doesNotHaveHelmet(event.getEntity())
-                    && event.getSource().getDamageLocation().y > headStart // Is the damage location the head?
-                    && event.getSource() != null // Is the source valid?
-                    && !((LivingEntity) entity).canBlockDamageSource(event.getSource())) { // Is the target blocking the projectile with a shield?
-                if (event.getEntity() instanceof AnimalEntity                       // Is the target quadrupedal?
-                        || event.getEntity() instanceof WaterMobEntity              // Is the target a water mob?
-                        || event.getEntity() instanceof SlimeEntity                 // Is the target a slime?
-                        || event.getEntity() instanceof EnderDragonEntity) return;  // Is the target the Ender Dragon?
-                if (event.getSource().getDamageLocation().y > headStart) { // Did the arrow hit the target's head?
-                    // Message sent to Player who made the headshot
-                    if (entity instanceof ServerPlayerEntity) {
-                        ((ServerPlayerEntity) entity).sendStatusMessage(new StringTextComponent("You got headshot!"),
-                                true);
+            if (doesNotHaveHelmet(event.getEntity())
+                    && event.getSource().getSourcePosition().y > headStart
+                    && event.getSource() != null
+                    && !((LivingEntity)entity).isDamageSourceBlocked(event.getSource())){
+                if (event.getEntity() instanceof Animal
+                        || event.getEntity() instanceof WaterAnimal
+                        || event.getEntity() instanceof Slime
+                        || event.getEntity() instanceof EnderDragon) return;
+                if (event.getSource().getSourcePosition().y > headStart) {
+                    if (entity instanceof ServerPlayer){
+                        ((ServerPlayer)entity).displayClientMessage(new TextComponent("You got headshot!"), true);
                     }
                 }
-                // Message sent to the Player who was headshot
-                if (event.getSource().getTrueSource() instanceof ServerPlayerEntity && trueSource != null) {
-                    ((ServerPlayerEntity) trueSource).sendStatusMessage(new StringTextComponent("Headshot!"),
-                            true);
+                if (event.getSource().getEntity() instanceof ServerPlayer && trueSource != null){
+                    ((ServerPlayer)trueSource).displayClientMessage(new TextComponent("Headshot!"), true);
                 }
-                // Headshot damage configuration
                 double headshotDamage = event.getAmount() * HeadshotConfig.HEADSHOT_DAMAGE_MULTIPLIER.get();
                 double projectileProtectionDamageReduction =
                         HeadshotConfig.HEADSHOT_PROJECTILE_PROTECTION_DAMAGE_REDUCTION.get();
 
-                if (hasProjectileProtection(event.getEntity())) { // Does the target have Projectile Protection? If so, do X
-                    event.setAmount((float) headshotDamage * (float) projectileProtectionDamageReduction); // Damage is halved with Projectile Protection
-
-                    /* Extra damage is done to helmets with a headshot.
-                     * With Projectile Protection, that damage is cut to 25% of
-                     * damage received.
-                     */
+                if (hasProjectileProtection(event.getEntity())){
+                    event.setAmount((float)  headshotDamage * (float) projectileProtectionDamageReduction);
                     event.getEntityLiving()
-                            .getItemStackFromSlot(EquipmentSlotType.HEAD)
-                            .attemptDamageItem(
-                                    ((int) headshotDamage / 4),
-                                    event.getEntity().getEntityWorld().rand,
+                            .getItemBySlot(EquipmentSlot.HEAD)
+                            .hurt(
+                                    (int) headshotDamage / 4,
+                                    event.getEntity().getCommandSenderWorld().random,
                                     null);
-                } else if (!(hasProjectileProtection(event.getEntity()))) { // Does the target have Projectile Protection? If not, to Y
-                    event.setAmount((float) headshotDamage); // Full damage without Projectile Protection
-
-                    /* Extra damage is done to helmets with a headshot.
-                     * Without Projectile Protection, that damage is cut to 50% of
-                     * damage received.
-                     */
+                } else if (!hasProjectileProtection(event.getEntity())){
+                    event.setAmount((float)headshotDamage);
                     event.getEntityLiving()
-                            .getItemStackFromSlot(EquipmentSlotType.HEAD)
-                            .attemptDamageItem(
-                                    ((int) headshotDamage / 2),
-                                    event.getEntity().getEntityWorld().rand,
+                            .getItemBySlot(EquipmentSlot.HEAD)
+                            .hurt((int) headshotDamage / 2,
+                                    event.getEntity().getCommandSenderWorld().random,
                                     null);
-                    ignore = true;
 
-                    // Optional Blindness Effect
-                    if (HeadshotConfig.DO_BLINDNESS.get()) {
-                        ((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.BLINDNESS,
+                    if (HeadshotConfig.DO_BLINDNESS.get()){
+                        ((LivingEntity)entity).addEffect(new MobEffectInstance(MobEffects.BLINDNESS,
                                 HeadshotConfig.BLIND_TICKS.get(), 3));
                     }
 
-                    // Optional Nausea Effect
-                    if (HeadshotConfig.DO_NAUSEA.get()) {
-                        ((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.NAUSEA,
-                                HeadshotConfig.NAUSEA_TICKS.get(), 2));
+                    if (HeadshotConfig.DO_NAUSEA.get()){
+                        ((LivingEntity)entity).addEffect(new MobEffectInstance(MobEffects.CONFUSION,
+                                HeadshotConfig.NAUSEA_TICKS.get(), 3));
                     }
-                    return;
                 }
-                ignore = false;
             }
         }
-
     }
     private static boolean doesNotHaveHelmet(Entity entity){
-        if (HeadshotConfig.HELMET_MITIGATION.get()) {
-            return ((LivingEntity) entity).getItemStackFromSlot(EquipmentSlotType.HEAD).isEmpty();
+        if (HeadshotConfig.HELMET_MITIGATION.get()){
+            return ((LivingEntity)entity).getItemBySlot(EquipmentSlot.HEAD).isEmpty();
         }
         return entity instanceof LivingEntity;
     }
 
     private static boolean hasProjectileProtection(Entity entity){
-        return EnchantmentHelper.getMaxEnchantmentLevel(Enchantments.PROJECTILE_PROTECTION, (LivingEntity) entity) > 0;
-
+        return EnchantmentHelper.getEnchantmentLevel(Enchantments.PROJECTILE_PROTECTION, (LivingEntity) entity) > 0;
     }
 }
